b9f1edf627d295c807cca6776390e000b0ad7f62
Mohammed Munshif <munshif.upcode@gmail.com>
2023-10-08 13:53:38 +0530

    added dynamic navlinks

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,3 +1,4 @@
+export const navliks = ['NEWS','HIGHLIGHTS','KNOCKOUT & GROUPS','SCORES & FIXTURES','TEAMS','FIFA WORLD CUP TROPHY TOUR','FIFA FAN FESTIVAL™']
 export const legancy = [
   {
     image: "https://digitalhub.fifa.com/transform/64248dbc-e853-4e56-8184-7f20505a46d2/FIFAPLS_BringTheNoise_THUMB_00?io=transform:fill,aspectratio:16x9,width:768&quality=75",
